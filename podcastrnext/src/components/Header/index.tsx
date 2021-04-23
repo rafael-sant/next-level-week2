70d6ab0f19fb3e34c45ce93ee3c40f835d1c3dd6
Rafael Barros <rafael.barros@netzee.com.br>
2021-04-23 00:15:54 -0300

    Criando estilos globais, configurando fonte, criando componente Header, player, configurando API em JSON, consumindo API na home, diferença enre SPA, SSR e SSG, gerando a home de forma estática, criando build do projeto

---
+++ podcastrnext/src/components/Header/index.tsx
@@ -0,0 +1,19 @@
+import ptBR from 'date-fns/esm/locale/pt/index.js';
+import format from 'date-fns/format';
+import styles from './styles.module.scss'
+
+export default function Header() {
+    const currentDate = format(new Date(), 'EEEEEE, d MMMM', {
+        locale: ptBR
+    });
+
+    return(
+        <header className={styles.headerContainer}> 
+            <img src="/logo.svg" alt="Logo"/>
+
+            <p>O melhor para você ouvir, sempre</p>
+
+            <span>{currentDate}</span>
+        </header>
+    )
+}
