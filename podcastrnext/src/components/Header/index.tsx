30b108986bea03adc063adb1a25b6354a29d635d
Rafael Barros <rafael.barros@netzee.com.br>
2021-04-25 14:43:29 -0300

    Página home, rotemamento com Next.js, página de episódio, páginas estáticas e dinâmicas

--- podcastrnext/src/components/Header/index.tsx
+++ podcastrnext/src/components/Header/index.tsx
@@ -1,11 +1,11 @@
-import ptBR from 'date-fns/esm/locale/pt/index.js';
+// import ptBR from 'date-fns/esm/locale/pt/index.js';
 import format from 'date-fns/format';
 import styles from './styles.module.scss'
 
 export default function Header() {
-    const currentDate = format(new Date(), 'EEEEEE, d MMMM', {
-        locale: ptBR
-    });
+    // const currentDate = format(new Date(), 'EEEEEE, d MMMM', {
+    //     locale: ptBR
+    // });
 
     return(
         <header className={styles.headerContainer}> 
@@ -13,7 +13,8 @@ export default function Header() {
 
             <p>O melhor para você ouvir, sempre</p>
 
-            <span>{currentDate}</span>
+            {/* <span>{currentDate}</span> */}
+            <span>Sex, 23 Abril</span>
         </header>
     )
 }
